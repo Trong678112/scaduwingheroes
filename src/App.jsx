50ef080c4f480601b93ae63e72a5a0d06c8a24d1
unknown <nguyentrongyun090@gmail.com>
2025-07-11 23:20:10 +0700

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
+// src/App.jsx  
 import React from 'react';  
-import { Routes, Route, useLocation } from 'react-router-dom';  
+import { Routes, Route } from 'react-router-dom';  
 import Header from './components/Header';  
 import Footer from './components/Footer';  
 import HomePage from './pages/HomePage';  
 import CardPage from './pages/CardPage';  
 import ScanPage from './pages/ScanPage';  
+import LibraryPage from './pages/LibraryPage'; // Thêm import mới  
 import './App.css';  
-import LibraryPage from './pages/LibraryPage';  
-function App() {  
-  const location = useLocation();  
-  const showHeader = !location.pathname.startsWith('/card/');  
   
+function App() {  
   return (  
     <div className="App">  
-      {showHeader && <Header />}  
-      <Routes>  
-        <Route path="/" element={<HomePage />} />  
-        <Route path="/scan" element={<ScanPage />} />  
-        <Route path="/card/:cardId" element={<CardPage />} />
-        <Route path="/library" element={<LibraryPage />} />
-      </Routes>  
+      <Header />  
+      <main className="main-content">  
+        <Routes>  
+          <Route path="/" element={<HomePage />} />  
+          <Route path="/scan" element={<ScanPage />} />  
+          <Route path="/card/:cardId" element={<CardPage />} />  
+          <Route path="/library" element={<LibraryPage />} /> {/* Route mới */}  
+        </Routes>  
+      </main>  
       <Footer />  
     </div>  
   );  
